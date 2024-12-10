4640cde3ccebba7698b3803dc344f1487b131ae3
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-10 19:42:43 +0000

    Fix participant foreign key

--- app/models/participant_collection_session.rb
+++ app/models/participant_collection_session.rb
@@ -1,4 +1,4 @@
 class ParticipantCollectionSession < ApplicationRecord
   belongs_to :collection
-  belongs_to :participant, class_name: "User::Participant"
+  belongs_to :participant, class_name: "User::Participant", foreign_key: "user_participant_id"
 end
