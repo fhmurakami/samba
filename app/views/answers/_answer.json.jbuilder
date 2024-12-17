a5c1fc3c121083ff2546333cb31ab027485949bb
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-17 01:10:50 +0000

    Remove unnecessary files

--- app/views/answers/_answer.json.jbuilder
+++
@@ -1,2 +0,0 @@
-json.extract! answer, :id, :user_participant_id, :equation_id, :answer_value, :correct_answer, :time, :created_at, :updated_at
-json.url answer_url(answer, format: :json)
--- db/20241125131011_create_join_table_group_user_participant.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableGroupUserParticipant < ActiveRecord::Migration[7.2]
-  def change
-    create_join_table :groups, :user_participants do |t|
-      t.index [ :group_id, :user_participant_id ]
-      t.index [ :user_participant_id, :group_id ]
-    end
-  end
-end
