c0a00d8f7c288fad592e4bfa14f2f5ea882ccdcf
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-17 03:18:19 +0000

    Add Participant model tests

--- spec/models/user/participant_spec.rb
+++ spec/models/user/participant_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe User::Participant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to have_db_column(:first_name).of_type(:string) }
+  it { is_expected.to have_db_column(:last_name).of_type(:string) }
+  it { is_expected.to have_db_column(:birth_date).of_type(:date) }
+
+  it { is_expected.to belong_to :user_admin }
+  it { is_expected.to belong_to :group }
+  it { is_expected.to have_many :answers }
+
+  describe "when creating a new User::Participant" do
+    it "is valid with valid attributes" do
+      expect(User::Participant.new(first_name: "John", last_name: "Doe", birth_date: 5.years.ago, group: build(:group), user_admin: build(:user_admin))).to be_valid
+    end
+
+    it "is not valid without a first name" do
+      expect(User::Participant.new(first_name: nil, last_name: "Doe", user_admin: build(:user_admin))).to_not be_valid
+    end
+
+    it "is not valid without a last name" do
+      expect(User::Participant.new(first_name: "John", last_name: nil, user_admin: build(:user_admin))).to_not be_valid
+    end
+
+    it "is not valid without a user_admin" do
+      expect(User::Participant.new(first_name: "John", last_name: "Doe", user_admin: nil)).to_not be_valid
+    end
+  end
+
+  describe "#full_name" do
+    it "returns the first and last name of the participant" do
+      participant = User::Participant.new(first_name: "John", last_name: "Doe")
+      expect(participant.full_name).to eq("John Doe")
+    end
+  end
 end
