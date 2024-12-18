1b4d4c12e4401f1293bebe51d3703588aa39fe21
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-18 14:42:59 +0000

    Add tests for User::Admin model

--- spec/models/user/admin_spec.rb
+++ spec/models/user/admin_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User::Admin, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'database columns' do
+    it { should have_db_column(:first_name).of_type(:string) }
+    it { should have_db_column(:last_name).of_type(:string) }
+    it { should have_db_column(:email).of_type(:string) }
+    it { should have_db_column(:encrypted_password).of_type(:string) }
+    it { should have_db_column(:reset_password_token).of_type(:string) }
+    it { should have_db_column(:reset_password_sent_at).of_type(:datetime) }
+    it { should have_db_column(:remember_created_at).of_type(:datetime) }
+    it { should have_db_column(:created_at).of_type(:datetime) }
+    it { should have_db_column(:updated_at).of_type(:datetime) }
+  end
+
+  describe 'model associations' do
+    it { should have_many(:participants).class_name('User::Participant') }
+    it { should have_many(:groups) }
+    it { should have_many(:collections) }
+    it { should have_many(:equations) }
+  end
+
+  describe 'model validations' do
+    it { should validate_presence_of(:email) }
+    it { should validate_uniqueness_of(:email).ignoring_case_sensitivity }
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_presence_of(:password) }
+    it { should validate_confirmation_of(:password) }
+  end
 end
