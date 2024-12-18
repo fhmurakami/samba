dd231f1dcf008960c1d01883406d944dd5e4a249
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-18 15:58:37 +0000

    Add tests for Group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,8 @@ class Group < ApplicationRecord
   belongs_to :user_admin, class_name: "User::Admin", dependent: :destroy
   has_many :participants, class_name: "User::Participant"
 
+  validates :name, presence: true
+
   def remove_participant(participant)
     participants.delete(participant)
   end
--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Group, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'database columns' do
+    it { should have_db_column(:name).of_type(:string) }
+    it { should have_db_column(:user_admin_id).of_type(:integer) }
+  end
+
+  describe 'model associations' do
+    it { should belong_to(:user_admin).class_name('User::Admin') }
+    it { should have_many(:participants).class_name('User::Participant') }
+  end
+
+  describe 'model validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  describe "#remove_participant" do
+    it "removes a participant from the group" do
+      group = create(:group)
+      participant = create(:user_participant, group: group)
+      expect(group.participants).to include(participant)
+      group.remove_participant(participant)
+      expect(group.participants).to_not include(participant)
+    end
+  end
 end
