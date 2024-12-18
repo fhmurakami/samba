8668008f336840e317753d8dd1b0c06f221a6711
Felipe Murakami <13895820+fhmurakami@users.noreply.github.com>
2024-12-18 16:19:44 +0000

    Add tests for Round model

--- spec/factories/rounds.rb
+++ spec/factories/rounds.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :round do
     collection
     association :participant, factory: :user_participant
-    started_at { "2024-12-06 14:52:50" }
-    completed_at { "2024-12-06 14:52:50" }
+    started_at { "2024-12-06 14:00:00" }
+    completed_at { "2024-12-06 14:02:50" }
   end
 end
